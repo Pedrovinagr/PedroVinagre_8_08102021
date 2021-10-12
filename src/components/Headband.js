c433abbb81c7bdb02e8b0866f0c9dab0843312cb
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-12 12:08:56 +0200

    continued

--- src/components/Headband.js
+++ src/components/Headband.js
@@ -7,7 +7,7 @@ function Sweatband(){
         <div className='body'>
             <div className='mask'>
                 <img src={picture} alt='img' className='picture' />
-                <h3 className='headline'>{headline}</h3>
+                <h1 className='headline'>{headline}</h1>
             </div>
         </div>
         
