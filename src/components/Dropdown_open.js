2ddf885cb6c8fed43fc3e02630e3a6988c11fc49
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-14 17:11:05 +0200

    continued

--- src/components/Dropdown_open.js
+++ src/components/Dropdown_open.js
@@ -2,52 +2,31 @@ import '../styles/F_logement.css';
 import vectorUp from '../assets/Vector_up.png';
 
 function DropdownOpen(){
-    const Reliability = 'Fiabilité'
-    const ContantReliability = 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes' 
-    const Respect = 'Respect'
-    const ContentRespect = 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
-    const Serve = 'Service'
-    const ContentServe = 'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.'
-    const Security = 'Sécurité'
-    const ContentSecurity = 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'
+    const description = 'Description'
+    const ContantDescription = 'Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l\'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d\'affaires. Vous êtes à1 station de la gare de l\'est (7 minutes à pied).' 
+    const equipements = 'Équipements'
+    const ContentEquipements = 'Climatisation Wi-Fi Cuisine Espace de travail Fer à repasser Sèche-cheveux Cintres'
     
     return (
         <div>
             <div>
                 <div className='dropdowns_up'>
-                    <p className='title_dropdown'>{Reliability}</p> 
+                    <p className='title_dropdown'>{description}</p> 
                     <img src={vectorUp} alt='kasa' className='vector_down' />
                 </div>
                 <div className='content'>
-                    <p className='text_content'>{ContantReliability}</p> 
+                    <p className='text_content'>{ContantDescription}</p> 
                 </div>
             </div>
 
             <div className='dropdowns_up'>
-                <p className='title_dropdown'>{Respect}</p> 
+                <p className='title_dropdown'>{equipements}</p> 
                 <img src={vectorUp} alt='kasa' className='vector_down' />
             </div>
             <div className='content'>
-                <p className='text_content'>{ContentRespect}</p> 
+                <p className='text_content'>{ContentEquipements}</p> 
             </div>
-            
-
-            <div className='dropdowns_up'>
-                <p className='title_dropdown'>{Serve}</p> 
-                <img src={vectorUp} alt='kasa' className='vector_down' />
-            </div>
-            <div className='content'>
-                <p className='text_content'>{ContentServe}</p> 
-            </div>
-            
-
-            <div className='dropdowns_up'>
-                <p className='title_dropdown'>{Security}</p> 
-                <img src={vectorUp} alt='kasa' className='vector_down' />
-            </div>
-            <div className='content'>
-                <p className='text_content'>{ContentSecurity}</p> 
-            </div>            
+                      
         </div>
     );
 } 
