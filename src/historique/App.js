264fc7956da1f9c20bae39551f784e25cb2bc4d8
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-18 14:32:43 +0200

    continued

---
+++ src/historique/App.js
@@ -0,0 +1,17 @@
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Dropdownlarge from '../components/Dropdownlarge';
+import Propos from '../components/App'
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Dropdownlarge />
+      <Propos />
+      <Footer />
+    </div>
+  );       
+}
+
+export default App;
