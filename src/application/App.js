2dab883a9aaa8d12613f02251c1202b2ab0c5d45
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-18 10:52:49 +0200

    continued

--- src/application/App.js
+++ src/application/App.js
@@ -1,12 +1,14 @@
 import Banner from '../components/Banner';
 import Footer from '../components/Footer';
 import Dropdownlarge from '../components/Dropdownlarge';
+import Propos from '../application/App_propos'
 
 function App() {
   return (
     <div>
       <Banner />
       <Dropdownlarge />
+      <Propos />
       <Footer />
     </div>
   );       
--- src/application/App_propos.js
+++ src/application/App_propos.js
@@ -1,19 +1,56 @@
-import Banner from '../components/Banner';
-import SweatbandPropos from '../components/Headband_propos';
-import Footer from '../components/Footer';
-import DropdownClose from '../components/Dropdownlarge_close';
-import DropdownOpen from '../components/Dropdownlarge_open';
+// import Banner from '../components/Banner';
+// import SweatbandPropos from '../components/Headband_propos';
+// import Footer from '../components/Footer';
+// import Dropdownlarge from '../components/Dropdownlarge';
+// import DropdownOpen from '../components/Dropdownlarge_open';
 
-function App() {
+const informationList = [
+    {
+      "id": "Drp01",
+      "title": "Fiabilité",
+      "description": "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    },
+    {
+      "id": "Drp02",
+      "title": "Respect",
+      "description": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      "id": "Drp03",
+      "title": "Service",
+      "description": "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    },
+    {
+      "id": "Drp04",
+      "title": "Sécurité",
+      "description": "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    }
+  ]
+
+function Dropdownlist() {
+
   return (
-    <div>
-      <Banner />
-      <SweatbandPropos />
-      <DropdownClose />
-      <DropdownOpen />
-      <Footer />
-    </div>
-  );       
+    <ul>
+      {informationList.map((title) => (
+        <li>{title}</li>
+      ))}
+    </ul>
+);       
 }
+export default Dropdownlist;
 
-export default App;
+
+
+/* <div>
+<Banner />
+<SweatbandPropos />
+<div>
+<div>
+  {informationList.map((information) => (
+    <div key={'${information}-${index}'}>{information}</div>
+  ))}
+</div>
+  <Dropdownlarge />
+</div>
+<Footer />
+</div> */
