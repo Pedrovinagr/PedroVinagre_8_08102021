250f02b2221d8e01709221eccbc769ed6ab17eae
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-18 14:33:02 +0200

    continued

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import Banner from './Banner';
+import SweatbandPropos from './Headband_propos';
+import Footer from './Footer';
+import Dropdownlarge from './Dropdownlarge';
+
+function App() {
+
+  return (
+    <div>
+      <Banner />
+      <SweatbandPropos />
+      <Dropdownlarge />
+      <Footer />
+    </div>
+  );       
+}
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+// import App from './application/App_logement';
 // import App from './application/App';
 // import App from './application/App_home';
 // import App from './application/App_404';
-import App from './application/App_propos';
-// import App from './application/App_logement';
-import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
