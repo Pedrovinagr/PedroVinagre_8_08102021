24650e82d116ad0f45ba787cf6e8df946f0fde23
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-19 10:04:52 +0200

    continued

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,13 +5,16 @@ import vectorUp from '../assets/Vector_up.png';
 import vectorDown from '../assets/Vector_down.png';
 
 function WarningBanner(props) {
+
+    const description = props.description
+    
     if (!props.warn) {
       return null;
     }
   
     return ( 
         <div className='content'>
-            <p className='text_content'>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à 1 station de la gare de l'est (7 minutes à pied).</p> 
+            <p className='text_content'>{description}</p> 
         </div>
     );
   }
@@ -32,11 +35,11 @@ function WarningBanner(props) {
     render() {
         const Up = <img src={vectorUp} alt='kasa' className='vector_up' />
         const Down = <img src={vectorDown} alt='kasa' className='vector_down' />
-        const description = 'Description'
+        const title = 'Description'
       return (
         <div className='dropdown'>
             <div className='dropdowns_up'>
-                <p className='title_dropdown'>{description}</p>
+                <p className='title_dropdown'>{title}</p>
                 <div className='btn'>
                     <button onClick={this.handleToggleClick} className='dropdown_btn'>
                         {this.state.showWarning ? Up : Down }
