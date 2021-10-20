ee57912195e1b52a35d1f223826198c02c59f367
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-20 20:31:44 +0200

    caroussel en cours

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,14 +1,30 @@
-import log_background from '../assets/Background.jpg';
+import logements from '../Datas/logements.json';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import '../styles/F_logement.css';
 
-function Carrousel(){
-    const pages = '1/4'
+function Carrousel(props){
+    const data = logements;
+    const picture = props.data.map((picture) => pictures)
+    console.log (picture)
+
     return (
-        <div className='carrousel'>
-            <img src={log_background} alt='img' className='img_background' />
-            <p className='pages'>{pages}</p>
-        </div>
-    );
+    <Carousel>
+        {data.map(pictures => (
+            <div key={pictures.id.toString()}>
+                <img src={pictures.cover} />
+            </div>
+        ))}
+    </Carousel>
+  )
 } 
 
 export default Carrousel
+
+  // const pages = '1/4'
+    // return (
+    //     <div className='carrousel'>
+    //         <img src={log_background} alt='img' className='img_background' />
+    //         <p className='pages'>{pages}</p>
+    //     </div>
+    // );
