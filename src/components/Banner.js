232c3a076facf76afdaaed653b8813bb10702913
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-25 11:19:40 +0200

    creat class

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,22 @@
+import { Component } from 'react';
 import LOGO from '../assets/LOGO_Banner.png';
 import '../styles/Banner.css';
 
-function Banner(){
-    
-    const NavHome = 'Accueil';
-    const NavNews = 'A propos'
-    return (
-        <div className='heater'>
-            <div>
-                <img src={LOGO} alt='kasa' className='logo' />
+class Banner extends Component(){
+
+    render(){
+        return (
+            <div className='heater'>
+                <div>
+                    <img src={LOGO} alt='kasa' className='logo' />
+                </div>
+                <div className='nav_container'>
+                    <a href className="nav home"><span>Accueil</span></a>
+                    <a href className="nav news"><span>A propos</span></a>
+                </div>
             </div>
-            <div className='nav_container'>
-                <a href className="nav home"><span>{NavHome}</span></a>
-                <a href className="nav news"><span>{NavNews}</span></a>
-            </div>
-        </div>
-        
-    );
-} 
+        );
+    }
+}
 
 export default Banner
