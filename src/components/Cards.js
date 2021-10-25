b7a3ca0f5e489ece00fe3fdc62f9986709cf8ff8
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-25 14:33:50 +0200

    continued

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logements from '../Datas/logements.json';
 import '../styles/Cards.css';
 
 class Cards extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {cards: card}
+        this.state = {cards: this.props.card}
     }
 
     render(){
