d8e3e2db24d2935d22f432d722bd5fb399677d73
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-25 15:43:35 +0200

    OK

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,19 +7,17 @@ class Cards extends React.Component {
         super(props)
         this.state = {card: this.props.card}
     }
-    
+
     render(){
         return (
             <div>
-                <ul className='log-list'>
-                    <div className='card'>
-                        <li key={this.state.card.id.toString()} className='img'> 
-                            <img className='lmj-plant-item-cover' src={this.state.card.cover} alt={`${this.state.card.id} cover`} />
-                        </li>
-                        <li className='title'>
-                            {this.state.card.title}
-                        </li>
-                    </div>)
+                <ul className='log-list card'>
+                    <li key={this.state.card.id.toString()} className='img'> 
+                        <img className='lmj-plant-item-cover' src={this.state.card.cover} alt={`${this.state.card.id} cover`} />
+                    </li>
+                    <li className='title'>
+                        {this.state.card.title}
+                    </li>
                 </ul>
 		    </div> 
         );
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Banner from '../components/Banner';
-// import logements from '../data/logements.json'
-// import Cards from '../components/Cards';
+import logements from '../data/logements.json'
+import Cards from '../components/Cards';
 import Footer from '../components/Footer';
 
 class App extends React.Component {
 
-    // constructeur(props) {
-    //     super(props);
-    //     this.props = {listItems: logements};
-    // }
+    constructeur(props) {
+        super(props);
+        this.state({listItems: logements});
+    }
 
     render() {
         return (
             <div>
                 < Banner />
-                {/* {this.state.listItems.map((item) =>
-                   < Cards card= {item} />)} */}
+                {this.state.listItems.map((item) =>
+                   < Cards card= {item} />)}
                 < Footer />
             </div>
         );
