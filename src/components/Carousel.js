67f4d48e496cdeedca0e72b7818b8540801b6e5a
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-27 11:12:44 +0200

    carousel creat

---
+++ src/components/Carousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { imageIndex: 0}
+        this.state = {imageArray: this.props.picture}
+
+    }
+}
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Carousel from '../components/Carousel';
 import Footer from '../components/Footer';
 
+import logements from '../Data/logements.json'
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {logement: logements.find(pictures => pictures.id === this.props.match.id("c67ab8a7"))}
+    }
 
     render() {
         return (
             <div>
                 < Banner />
+                <Carousel picture = {this.state.logement.pictures} />
                 < Footer />
             </div>
         );
