0be9e8c250545f4d6d8ba5108fcf4de869fea6e1
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-29 11:59:08 +0200

    continued

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './pages/Propos';
+import App from './pages/Logement';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import '../styles/Logement.css'
+
 import Banner from '../components/Banner';
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
+import Dropdown from '../components/Dropdown'
 import Footer from '../components/Footer';
 
-import logements from '../Data/logements.json'
+// import logements from '../Data/logements.json'
+
+// const Logements = require("../Data/logements.json");
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.State({logement: logements.find(picture => picture.id ("c67ab8a7"))})
-        console.log(this.state.logement)
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.State({logement: logements.find(picture => picture.id ("c67ab8a7"))})
+    //     console.log(this.state.logement)
+    // }
 
     render() {
         return (
             <div>
                 < Banner />
-                <Carousel picture = {this.state.logement.cover} />
+                {/* <Carousel picture = {this.state.logement.cover} /> */}
+                <div>
+                    < Dropdown />
+                    < Dropdown />
+                </div>
+
                 < Footer />
             </div>
         );
