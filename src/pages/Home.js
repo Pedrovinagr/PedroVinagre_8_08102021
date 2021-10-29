b8479e402a012aa4008e8165a6821e39eb633c05
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-10-29 09:42:06 +0200

    home en cours

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Logements from '../data/logements.json';
+import Logements from '../data/logements.json';
 import Picture from '../assets/IMG.jpg';
 
 import Banner from '../components/Banner';
@@ -8,19 +8,21 @@ import Cards from '../components/Cards';
 import Footer from '../components/Footer';
 class App extends React.Component {
 
-    // constructeur(props) {
-    //     super(props);
-    //     this.setState({listItems: Logements});
-    // }
-
+    constructeur(props) {
+        super(props);
+        this.State = {listItems: Logements};
+        console.log(this.state.listItems)
+    }
+    
     render() {
+        
+        console.log(listItems)
         return (
             <div>
                 < Banner />
                 < Headband picture = {Picture} headline = "Chez vous, partout et ailleurs" />
-                {/* {this.state.listItems.map((item) =>
-                   < Cards card= {item} />)} */}
-                < Cards />
+                {this.state.listItems.map((item) => < Cards card= {item} />)}
+                {/* < Cards /> */}
                 < Footer />
             </div>
         );
