12a5d1a5b052ac2c3ebda3f2a90aa679adf27f8d
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-02 14:31:18 +0100

    carousel continued

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,35 +3,39 @@ import '../styles/Carousel.css'
 // import React, {useState} from "react";
 import ChevronRight from '../assets/ChevronRight.png'
 import ChrevronLeft from '../assets/ChevronLeft.png'
-import { useState } from "react/cjs/react.development";
 class Carousel extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {slides: this}
-    }
-
-
+        this.state = { 
+            picturesIndex: 0,
+            picturesArray: this.props.picture,
+            pictures: this.props.picture[0]};
+        }
     render() {
-        const [current, setCurrent] = useState(0);
-        const length = slides.length;
-
         const nextSlide = () => {
-            setCurrent(current === length -1 ? 0 : current +1)
+            if(this.state.picturesIndex < this.state.picturesArray.length) {
+                this.setState({picturesIndex : picturesIndex + 1})
+            }
+            else {
+                this.setState({picturesIndex: 0})
+            }
+
+            this.setState({ pictures: this.state.picturesArray[this.state.picturesIndex]});
         }
 
-        console.log(current)
-
-        if(!Array.isArray(slides) || slides.length <= 0) {
-            return null;
+        const prevSlide = () => {
+            if (this.state.picturesIndex > 0) {
+                this.setState({picturesIndex : picturesIndex -1});
+                
+            }
         }
-
+        
         return(
             <section className='carousel'>
                 <img src={ChrevronLeft} alt='left-Arrow' className='Chrevron left' />
-                <img src={ChevronRight} alt='right-Arrow' className='Chrevron right' onClick={nextSlide}/>
-                {this.props.picture.map((pictures) => <img src={pictures} alt='kasa' className='house_image' />)} 
-                <p> {this.props.picture.description}</p>
+                <img src={ChevronRight} alt='right-Arrow' className='Chrevron right' onClick={nextSlide} />
+                {this.props.picture.map((pictures, index) => <img key={index} src={pictures} alt='kasa' className='house_image' />)}
             </section>
         )
     }
@@ -41,16 +45,18 @@ export default Carousel;
 
 
 
-// this.state = { picturesIndex: 0};
-        // this.state = {picturesArray: this.props.picture};
-        // this.state = {pictures: this.props.picture[0]};
-    // nextPictures = () => {
-    //     if(this.state.picturesIndex < this.state.picturesArray.length) {
-    //         this.setState({picturesIndex : this.state.picturesIndex + 1})
-    //     }
-    //     else {
-    //         this.setState({picturesIndex : 0})
-    //     }
+// this.state = {current: 0}
+//         this.state = {index: 0}
+//     }
 
-    //     this.setState({ pictures: this.state.picturesArray[this.state.picturesIndex]});
-    // }
+//     render() {
+//         const length = this.props.picture.length;
+//         const nextSlide = () => {
+//             this.setState(this.state.current === length -1 ? 0 : this.state.current +1)
+//         }
+
+//         console.log(this.state.current)
+
+//         if(!Array.isArray(this.props.picture) || this.props.picture.length <= 0) {
+//             return null;
+//         }
