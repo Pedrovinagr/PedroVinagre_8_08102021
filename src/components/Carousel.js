a32e169bbea050d9c4c6e167d0655753ba3bf755
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-02 12:42:06 +0100

    carousel continued

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,49 @@
 import React from "react";
-
+import '../styles/Carousel.css'
+// import React, {useState} from "react";
+import ChevronRight from '../assets/ChevronRight.png'
+import ChrevronLeft from '../assets/ChevronLeft.png'
+import { useState } from "react/cjs/react.development";
 class Carousel extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { picturesIndex: 0};
-        this.state = {picturesArray: this.props.picture};
-        this.state = {pictures: this.props.picture[0]};
+        this.state = {slides: this}
     }
 
+
+    render() {
+        const [current, setCurrent] = useState(0);
+        const length = slides.length;
+
+        const nextSlide = () => {
+            setCurrent(current === length -1 ? 0 : current +1)
+        }
+
+        console.log(current)
+
+        if(!Array.isArray(slides) || slides.length <= 0) {
+            return null;
+        }
+
+        return(
+            <section className='carousel'>
+                <img src={ChrevronLeft} alt='left-Arrow' className='Chrevron left' />
+                <img src={ChevronRight} alt='right-Arrow' className='Chrevron right' onClick={nextSlide}/>
+                {this.props.picture.map((pictures) => <img src={pictures} alt='kasa' className='house_image' />)} 
+                <p> {this.props.picture.description}</p>
+            </section>
+        )
+    }
+}
+
+export default Carousel;
+
+
+
+// this.state = { picturesIndex: 0};
+        // this.state = {picturesArray: this.props.picture};
+        // this.state = {pictures: this.props.picture[0]};
     // nextPictures = () => {
     //     if(this.state.picturesIndex < this.state.picturesArray.length) {
     //         this.setState({picturesIndex : this.state.picturesIndex + 1})
@@ -19,14 +54,3 @@ class Carousel extends React.Component {
 
     //     this.setState({ pictures: this.state.picturesArray[this.state.picturesIndex]});
     // }
-
-    render() {
-        return(
-            <div>
-                <img src={this.props.picture} alt='kasa' className='logo' />
-            </div>
-        )
-    }
-}
-
-export default Carousel;
